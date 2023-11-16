03f12a3fdaf325c2778ac0b1ea09d20857ef1109
Shahab Ziaeian <shahabziaeian@yahoo.de>
2023-11-16 16:32:09 +0100

    added StyledImage

--- components/RecipeDetails.js
+++ components/RecipeDetails.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function RecipeDetails({ recipeData }) {
   return (
-    <>
+    <RecipeCard>
       <h1> {recipeData.name}</h1>
-      <Image
+      <StyledImage
         src={recipeData.imageURL}
         alt={recipeData.name}
         width={300}
@@ -20,8 +20,16 @@ export default function RecipeDetails({ recipeData }) {
         ))}
       </ul>
       <p>{recipeData.description}</p>
-    </>
+    </RecipeCard>
   );
 }
 
-const RecipeCard = styled.article``;
+const RecipeCard = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledImage = styled(Image)`
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`;
